38cdd6bd77b51a5a09b095bea1c7bfd19d93489d
Thomas Modave <85135970+The-Bear50@users.noreply.github.com>
2024-04-12 11:24:02 +0200

    Create iftop_network_monitoring.sh

---
+++ Linux/03_El_Doctor_own_monitoring_script/Scripts/iftop_network_monitoring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Directory where the output will be saved
+output_dir="/home/library/Linux/Monitoring/scripts/Outputs"
+
+# File to store the output
+output_file="$output_dir/network_load_output.txt"
+
+# Trigger iostat -h command and redirect output to file
+sudo iftop -t -s 10 > "$output_file"
+
+# Print a message indicating where the output is saved
+echo "Network current load saved to $output_file"
